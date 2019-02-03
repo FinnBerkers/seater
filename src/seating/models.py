83810384eda75bc93e0207525e05af68e9eae19f
Abby Berkers <abbyberkers@gmail.com>
2019-02-03 21:16:23 +0100

    Added forbidden neighbor relation to the database

--- src/seating/models.py
+++ src/seating/models.py
@@ -2,9 +2,6 @@ from django.db import models
 
 
 class Family(models.Model):
-    """
-
-    """
     name = models.CharField(max_length=100)
 
     def __str__(self):
@@ -15,6 +12,11 @@ class Person(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     family = models.ForeignKey(Family, on_delete=models.CASCADE)
+    # A relation to itself (other instances). Here we specify the
+    # forbidden neighbors of this person.
+    # If we add a forbidden neighbor here, this person is automatically
+    # added to the forbidden neighbors of the selected neighbor (yay).
+    forbidden_neighbors = models.ManyToManyField("self", blank=True)
 
     def __str__(self):
-        return self.first_name
+        return "{} {}".format(self.first_name, self.last_name)
